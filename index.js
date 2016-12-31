5fec1f00223715c984552281a283b7500f923995
Yoshihisa Ozaki <yoozaki@tohmatsu.co.jp>
2016-12-31 09:58:42 +0900

    update

--- index.js
+++ index.js
@@ -2,8 +2,4 @@ const shell = require('electron').shell
 
 const os = require('os')
 
-const fileManagerBtn = document.getElementById('open-file-manager')
-
-fileManagerBtn.addEventListener('click', function(event) {
-    shell.showItemInFolder(os.homedir())
-})
+shell.showItemInFolder(os.homedir())
